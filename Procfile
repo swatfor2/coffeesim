db8b8c6fc9fdf45ff678512843763e2882075417
Max Hänsel <mh@maxhaensel.de>
2019-02-27 18:33:07 +0100

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python -m flask run
+web: gunicorn -b 0.0.0.0:$PORT app:app
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask import render_template
 from user_interface import next
@@ -40,6 +42,6 @@ def deleteSimulationCall():
         return "Alle Daten geloescht"
 
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
